5229e91de0b9135ab31d6af0e18786a116a9e351
diep <dieptran.090704@gmail.com>
2025-04-22 18:56:19 +0700

    2

---
+++ APITraTuDien1/APITraTuDien/src/main/java/com/sqc/APITraTuDien/ApiTraTuDienApplication.java
@@ -0,0 +1,13 @@
+package com.sqc.APITraTuDien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTraTuDienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTraTuDienApplication.class, args);
+	}
+
+}
---
+++ APITraTuDien1/APITraTuDien/src/test/java/com/sqc/APITraTuDien/ApiTraTuDienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqc.APITraTuDien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTraTuDienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
